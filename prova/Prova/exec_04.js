534fe9df92677160a958a87fa74f84e98b1f269c
Arthur Andrade <120082347+arthraw@users.noreply.github.com>
2023-06-14 10:42:40 -0300

    Update exec_04.js

--- prova/Prova/exec_04.js
+++ prova/Prova/exec_04.js
@@ -4,13 +4,12 @@
 Um quadrado perfeito é um número inteiro cuja raiz quadrada também é um número inteiro. 
 Por exemplo, 25 é um quadrado perfeito porque a sua raiz quadrada é 5, um número inteiro.
 
-Ex: 8 % 64 = 8
-
 */ 
 
 function quadradoPerfeito(numQuadrado, numAuxiliar) 
 {
-    if (numAuxiliar % numQuadrado === numAuxiliar &&  ) 
+    
+    if (numQuadrado / numAuxiliar  === numAuxiliar) 
     {
         console.log("É um quadrado perfeito.")
     } else 
@@ -18,3 +17,15 @@ function quadradoPerfeito(numQuadrado, numAuxiliar)
         console.log("Não é um quadrado perfeito.")
     }
 }
+
+/*
+    O código acima retorna com base nos valores recebidos se o numQuadrado é ou não um quadrado perfeito, 
+    ele faz isso por meio no numAuxiliar que serve para para fazer a divisão do numQuadrado.
+    
+    Por exemplo:
+
+    quadradoPerfeito(25, 5)
+
+    --> Ira retorna "É um quadrado perfeito." ja que 25 dividido por 5 resulta em um quadrado perfeito
+
+ */
